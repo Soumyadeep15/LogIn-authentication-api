72bfab9e5087fb379838d67e3b25543ef339fddc
Soumya's PC <soumyasaharoy@gmail.com>
2023-12-18 15:34:26 +0530

    now passwords are encrypted in database using bcrypt.js

--- controller/index.js
+++ controller/index.js
@@ -1,29 +1,44 @@
 const db = require('../database')
+const bcrypt = require('bcrypt')
 const user = db.user
 
-const createNewUser = async(req, res) => {
-    const {email, password} = req.body
-    await user.create({email, password})
+const createNewUser = async (req, res) => {
+    let { email, password } = req.body
+    const hashedPassword = bcrypt.hashSync(password, 10)
+
+    password = hashedPassword
+    
+    await user.create({ email, password })
     res.status(200).json({
         message: 'sign in successfully'
     })
 }
 
-const authenticate = async(req, res) => {
-    const {email, password} = req.body
+const authenticate = async (req, res) => {
+    const { email, password } = req.body
 
-    const data = await user.findOne({where: {email}})
+    const data = await user.findOne({ where: { email } })
 
-    if(!data) res.status(200).json({ message: 'invalid'})
+    if (!data) res.status(200).json({ message: 'invalid email' })
 
-    if(password === data.password) 
+    // if(password === data.password) 
+    //     res.status(200).json({
+    //         message: 'user login successfully',
+    // })
+    // else 
+    //     res.status(200).json({
+    //         message: 'wrong email or password',
+    // })
+    const isPasswordValid = await bcrypt.compare(password, data.password)
+
+    if (isPasswordValid)
         res.status(200).json({
-            message: 'user login successfully',
-    })
-    else 
+            message: 'user login successfully'
+        })
+    else
         res.status(200).json({
             message: 'wrong email or password',
-    })
+        })
 
 }
 
